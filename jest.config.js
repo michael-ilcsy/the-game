2685a6bdbb474f2df200783ea173897da4c93e29
michael-ilcsy <michael_ilcsy_git@yahoo.co.jp>
2019-04-16 11:35:12 +0900

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'json',
+    'vue',
+    'ts',
+    'tsx'
+  ],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  transformIgnorePatterns: [
+    '/node_modules/'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  snapshotSerializers: [
+    'jest-serializer-vue'
+  ],
+  testMatch: [
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
+  ],
+  testURL: 'http://localhost/',
+  watchPlugins: [
+    'C:\\Git\\社外開発\\個人開発\\the-game\\node_modules\\jest-watch-typeahead\\filename.js',
+    'C:\\Git\\社外開発\\個人開発\\the-game\\node_modules\\jest-watch-typeahead\\testname.js'
+  ],
+  globals: {
+    'ts-jest': {
+      babelConfig: true
+    }
+  }
+}
