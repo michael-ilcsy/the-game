ae0858fc113a6ccee4f37263cc429b92c744a059
michael-ilcsy <michael_ilcsy_git@yahoo.co.jp>
2019-04-16 18:00:26 +0900

    feat: 簡単に手札とカードのvue作成

---
+++ src/components/Card.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card">
+    <div class="number">
+      {{ number }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import {Component, Prop, Vue} from "vue-property-decorator"
+
+  @Component()
+  export default class Card extends Vue {
+    @Prop({required: true})
+    public number!: number
+  }
+</script>
+
+<style scoped lang="scss">
+  .card {
+    width: 100px;
+    height: 140px;
+    border: #444 solid 1px;
+
+    box-sizing: border-box;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    font-size: 3rem;
+  }
+</style>
---
+++ src/components/Hand.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hand">
+    <label :for="`card${index}`" v-for="(number,index) in cardNumbers">
+      <input type="radio" :id="`card${index}`" name="card">
+      <Card :number="number"></Card>
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+  import {Component, Prop, Vue} from "vue-property-decorator"
+  import Card from "@/components/Card.vue"
+
+  @Component({
+    components: {
+      Card
+    }
+  })
+  export default class Hand extends Vue {
+    public cardNumbers: number[] = [1, 2, 12, 34, 45, 56, 82, 99]
+  }
+</script>
+
+<style scoped lang="scss">
+  .hand {
+    display: flex;
+  }
+
+  input[type="radio"] {
+    display: none;
+  }
+
+  input[type="radio"]:checked + .card {
+    border: 4px solid #ff9900;
+  }
+</style>
